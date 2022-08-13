99c9004aea432810ff53dbf08f7260b06d0c681d
tomasstankevic <rezonansas@fidi.lt>
2022-08-13 23:50:55 +0200

    Runs weather panel on raspberry pi

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 source scripts/config.env
 
-ssh $HOSTNAME "mkdir -p $VENV_ROOT_DIRECTORY"
-rsync -av --delete requirements.txt $HOSTNAME:$VENV_ROOT_DIRECTORY
-ssh $HOSTNAME "cd $VENV_ROOT_DIRECTORY && python -m venv $VENV_NAME"
-ssh $HOSTNAME "source $VENV_DIRECTORY/bin/activate && pip install -r $VENV_ROOT_DIRECTORY/requirements.txt && rm $VENV_ROOT_DIRECTORY/requirements.txt"
+#mkdir -p $VENV_ROOT_DIRECTORY
+#rsync -av --delete requirements.txt $HOSTNAME:$VENV_ROOT_DIRECTORY
+#cd $VENV_ROOT_DIRECTORY && python -m venv $VENV_NAME
+#source $VENV_DIRECTORY/bin/activate && pip install -r $VENV_ROOT_DIRECTORY/requirements.txt && rm $VENV_ROOT_DIRECTORY/requirements.txt
